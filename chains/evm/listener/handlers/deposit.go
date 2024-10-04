843dbcc26939a80d01fc7bd3e6d3240a110c308b
mpetrun5 <matija.petrunic@gmail.com>
2024-10-04 14:09:39 +0200

    Prevent double handling of events

--- chains/evm/listener/handlers/deposit.go
+++ chains/evm/listener/handlers/deposit.go
@@ -10,6 +10,7 @@ import (
 	"math/big"
 	"slices"
 	"strings"
+	"sync"
 
 	ethereumABI "github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
@@ -55,6 +56,7 @@ type DepositEventHandler struct {
 	slotIndex        uint8
 	genericResources []string
 	msgChan          chan []*message.Message
+	lock             sync.Mutex
 }
 
 func NewDepositEventHandler(
@@ -74,10 +76,14 @@ func NewDepositEventHandler(
 		genericResources: genericResources,
 		msgChan:          msgChan,
 		domainID:         domainID,
+		lock:             sync.Mutex{},
 	}
 }
 
 func (h *DepositEventHandler) HandleEvents(destination uint8, startBlock *big.Int, endBlock *big.Int, slot *big.Int) error {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+
 	deposits, err := h.fetchDeposits(destination, startBlock, endBlock)
 	if err != nil {
 		return err
